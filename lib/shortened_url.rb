e3c99ffe7a7d59a87ef992fd15d842e649b45e6f
quest <quest@mac.com>
2011-04-05 10:35:59 -0700

    Check presence before uniqueness

--- lib/shortened_url.rb
+++ lib/shortened_url.rb
@@ -1,8 +1,8 @@
 # Class to store necessary info for shortening. 
 # Just a url to shorten, and an id. so simple.
 class ShortenedUrl < ActiveRecord::Base
-  validates_uniqueness_of :url
   validates_presence_of :url
+  validates_uniqueness_of :url
   validates_format_of :url, :with => /^\b((?:https?:\/\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))$/
   
   # Shortens an ID by using alphadecimal format (base62)
