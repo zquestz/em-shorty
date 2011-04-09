772c11a8b1afd69992af11093233e9c4d1b9515c
quest <quest@mac.com>
2011-04-09 04:51:00 -0700

    Fixed rest of specs

--- test/test_hashify.rb
+++ test/test_hashify.rb
@@ -14,24 +14,24 @@ class TestHashify < Test::Unit::TestCase
     assert_equal Digest::SHA2.hexdigest('hi'), 'hi'.to_sha2
   end
   
-  def hashify_default_sha1
+  def test_hashify_default_sha1
     assert_equal 'hi'.hashify, 'hi'.hashify(:sha1)
   end
   
-  def hashify_md5
-    assert_equal Digest::MD5.hexdigest('hi'), 'hi'.to_hashify(:md5)
+  def test_hashify_md5
+    assert_equal Digest::MD5.hexdigest('hi'), 'hi'.hashify(:md5)
   end
   
-  def hashify_sha1
-    assert_equal Digest::SHA1.hexdigest('hi'), 'hi'.to_hashify(:sha1)
+  def test_hashify_sha1
+    assert_equal Digest::SHA1.hexdigest('hi'), 'hi'.hashify(:sha1)
   end
   
-  def hashify_sha2
-    assert_equal Digest::SHA2.hexdigest('hi'), 'hi'.to_hashify(:sha2)
+  def test_hashify_sha2
+    assert_equal Digest::SHA2.hexdigest('hi'), 'hi'.hashify(:sha2)
   end
   
-  def hashify_invalid
-    assert_nil 'hi'.to_hashify(:error)
+  def test_hashify_invalid
+    assert_nil 'hi'.hashify(:error)
   end
   
 end
