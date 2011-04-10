49daeca82aa96cecab6a20ceee4e3f1324551b7d
quest <quest@mac.com>
2011-04-09 18:38:24 -0700

    Modified Hashify to be a module for any class

--- lib/hashify.rb
+++ lib/hashify.rb
@@ -1,22 +1,52 @@
 # Add popular hashing functions to strings.
 # Then wrap in a nice hashify method.
-class String
+
+module Hashify
+  
   VALID_FORMATS = [:md5, :sha1, :sha2]
   
-  def hashify(format = :sha1)
-    self.send("to_#{format}") if VALID_FORMATS.include?(format)
+  def self.included(included_class)
+    included_class.class_eval do
+      def self.default_hash
+        @@default_hash ||= :sha1
+      end
+
+      def self.default_hash=(hash_type)
+        @@default_hash = hash_type if VALID_FORMATS.include?(format)
+      end
+
+      def hashify_string
+        "#{self.class}_#{self}"
+      end
+
+      def hashify(format = self.class.default_hash)
+        self.send("to_#{format}") if VALID_FORMATS.include?(format)
+      end
+
+      def to_md5
+        Digest::MD5.hexdigest(hashify_string)
+      end
+
+      def to_sha1
+        Digest::SHA1.hexdigest(hashify_string)
+      end
+
+      def to_sha2
+        Digest::SHA2.hexdigest(hashify_string)
+      end
+    end
   end
-  
-  def to_md5
-    Digest::MD5.hexdigest(self)
-  end
-  
-  def to_sha1
-    Digest::SHA1.hexdigest(self)
-  end
-  
-  def to_sha2
-    Digest::SHA2.hexdigest(self)
-  end
-  
+
+end
+
+class String
+  include Hashify
+end
+
+class Array
+  include Hashify
+end
+
+class Hash
+  include Hashify
 end
--- test/test_hashify.rb
+++ test/test_hashify.rb
@@ -2,32 +2,32 @@ require 'helper'
 
 class TestHashify < Test::Unit::TestCase
 
-  def test_to_md5
-    assert_equal Digest::MD5.hexdigest('hi'), 'hi'.to_md5
+  def test_string_to_md5
+    assert_equal Digest::MD5.hexdigest('String_hi'), 'hi'.to_md5
   end
   
-  def test_to_sha1
-    assert_equal Digest::SHA1.hexdigest('hi'), 'hi'.to_sha1
+  def test_string_to_sha1
+    assert_equal Digest::SHA1.hexdigest('String_hi'), 'hi'.to_sha1
   end
   
-  def test_to_sha2
-    assert_equal Digest::SHA2.hexdigest('hi'), 'hi'.to_sha2
+  def test_string_to_sha2
+    assert_equal Digest::SHA2.hexdigest('String_hi'), 'hi'.to_sha2
   end
   
   def test_hashify_default_sha1
     assert_equal 'hi'.hashify, 'hi'.hashify(:sha1)
   end
   
-  def test_hashify_md5
-    assert_equal Digest::MD5.hexdigest('hi'), 'hi'.hashify(:md5)
+  def test_hashify_string_md5
+    assert_equal Digest::MD5.hexdigest('String_hi'), 'hi'.hashify(:md5)
   end
   
-  def test_hashify_sha1
-    assert_equal Digest::SHA1.hexdigest('hi'), 'hi'.hashify(:sha1)
+  def test_hashify_string_sha1
+    assert_equal Digest::SHA1.hexdigest('String_hi'), 'hi'.hashify(:sha1)
   end
   
-  def test_hashify_sha2
-    assert_equal Digest::SHA2.hexdigest('hi'), 'hi'.hashify(:sha2)
+  def test_hashify_string_sha2
+    assert_equal Digest::SHA2.hexdigest('String_hi'), 'hi'.hashify(:sha2)
   end
   
   def test_hashify_invalid
